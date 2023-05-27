0500dae90351515c434cafbee6736b469423b047
666wCodes <134250441+666wCodes@users.noreply.github.com>
2023-05-27 13:24:18 +0800

    added

--- commands/panel.js
+++ commands/panel.js
@@ -5,9 +5,30 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   name: 'panel',
   description: 'Create a panel where users can get a role or nickname change when they connected their roblox account.',
-  addChannelOption(option => option.setName('channel').setDescription('Where the panel will send to')),
-  addRoleOption(option => option.setName('role').setDescription('The role that will be given to users who connected their roblox account')),
-  addBooleanOption(option => option.setName('change_nickname').setDescription('Changes user\'s nickname to their Roblox account name')),
+  options: [
+    {
+      name: 'channel',
+      description: 'Channel to send panel message',
+      type: 'ROLE',
+      required: false,
+    },
+    {
+      name: 'role',
+      description: 'Role to add to Connected users',
+      type: 'ROLE',
+      required: false,
+    },
+    {
+      name: 'change_nickname',
+      description: 'Changes user\'s nickname to their connected Roblox name',
+      type: 'BOOLEAN',
+      required: true,
+    },
+  ],
   async execute(interaction) {
+    let role = interaction.options.getRole('role');
+    let changeNick = interaction.options.getBoolean('change_nickname');
+    
+    
   }
   }
