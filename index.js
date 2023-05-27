f28d1117e032570af7cf5e3c2e1a94bd65465228
666wCodes <134250441+666wCodes@users.noreply.github.com>
2023-05-27 19:08:47 +0800

    fixed to delete and register commands

--- index.js
+++ index.js
@@ -17,74 +17,70 @@ app.listen(PORT, () => {
 
 //WHERE EXPRESS ENDS
 
-const { Client, Collection, Intents, Discord } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 const fs = require('fs');
-const client = new Client({ 
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES]
-});
+const client = new Client({ intents: [Intents.FLAGS.Guilds, Intents.FLAGS.GuildMessages] });
 
 // Create a collection to store the slash commands
 client.commands = new Collection();
 
-client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}`);
-  registerSlashCommands();
-});
+// Read the contents of the /commands folder
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('interactionCreate', async (interaction) => {
-    if (interaction.isModalSubmit()){
-const roblox = interaction.fields.getTextInputValue('roblox');
-    let isverified = db.get(`roblox-verified-${roblox}`)
-	if(isverified !== null) return interaction.reply({ content: "This Roblox account is already connected to another user, or yours, to change it, do "})
-    let robloxusername = db.get(`roblox-name-${interaction.member.id}`)
-    let robloxuserid = db.get(`roblox-id-${interaction.member.id}`)
-	let session = db.get(`discord-session-${interaction.member.id}`)
-    let robloxsession = db.get(`roblox-session-${roblox}`)
-        
-        const embed = new Discord.MessageEmbed()
-        .setTitle(`Hello ${roblox}`)
-        .addFields({ name: "Please verify your discord account by joining the roblox game below with your roblox account", value: `**${gamelink}**`})
-		.setTimestamp()
-        .setFooter("If you need support, don\'t be afraid to join our Discord Server")
-        await interaction.reply({ content: `This session expires in 10 minutes.`, embeds: [embed], ephemeral: true });
-    }
-  if (!interaction.isCommand()) return;
-
-  const { commandName } = interaction;
-
-  // Find and execute the matching slash command
-  const command = client.commands.get(commandName);
-  if (command) {
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(`Error executing command ${commandName}:`, error);
-      await interaction.reply({
-        content: 'Something happened when you use this command, try again?',
-        ephemeral: true,
-      });
-    }
-  }
-});
-
-// Function to register all slash commands globally
-async function registerSlashCommands() {
-  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-  for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-
-    // Create the global slash command
-    const commandData = {
-      name: command.name,
-      description: command.description,
-      options: command.options,
-    };
-
-    await client.application.commands.create(commandData);
-    console.log(`Created command ${command.name}`);
-  }
+// Dynamically register slash commands
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
 }
+
+// Bot token and client ID
+const token = process.env.BOT_TOKEN;
+const clientId = 1106842998345568306;
+
+// Create a REST client for registering slash commands
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    // Fetch all the registered slash commands globally
+    const commands = await rest.get(
+      Routes.applicationGuildCommands(clientId, 'GLOBAL')
+    );
+
+    // Get the IDs of the existing slash commands
+    const existingCommandIds = commands.map(command => command.id);
+
+    // Filter out the commands that should be deleted
+    const commandsToDelete = commands.filter(command =>
+      !client.commands.has(command.name)
+    );
+
+    // Delete the unnecessary commands
+    for (const command of commandsToDelete) {
+      await rest.delete(
+        Routes.applicationGuildCommand(clientId, 'GLOBAL', command.id)
+      );
+    }
+
+    // Register or update the slash commands
+    const newCommands = client.commands.map(command => command.data.toJSON());
+    const commandsToRegister = newCommands.filter(command =>
+      !existingCommandIds.includes(command.id)
+    );
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, 'GLOBAL'),
+      { body: commandsToRegister }
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 client.login(process.env.BOT_TOKEN);
