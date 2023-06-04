432d483581b11c0116678a672d14350d30b2483a
666wCodes <134250441+666wCodes@users.noreply.github.com>
2023-06-04 18:47:08 +0800

    eee

--- commands/help.js
+++ commands/help.js
@@ -26,15 +26,21 @@ module.exports = {
 
       let commandString = "";
       for (let i = 0; i < commandList.length; i++) {
-      commandString = commandString + `\n**${bullet} [${commandList[i].name}](https://printer.discord.com)**\n<:arrow:1114860819881144390> ${commandList[i].desc}\n<:WatchuDoinMyGuy:1114864975874367519>`
+      commandString = commandString + `\n**${bullet} [${commandList[i].name}](https://printer.discord.com)**\n<:arrow:1114860819881144390> ${commandList[i].desc}`
       }
 
         //const line = `<a:line:1114860744765354065>`
         //const linelist = `${line}${line}${line}${line}${line}${line}${line}${line}${line}${line}`
+        let guide = `
+1 ${bullet} Create a panel with \`/create\` with your desired options
+2 ${bullet} Enable or set settings with \`/settings\`
+3 ${bullet} After all is done, your server should already be configured!`
         let embed = new MessageEmbed()
         .setTitle("Help Center")
         //.setDescription(linelist)
-        .addFields({ name: `<:info:1114861006502498376> ${bullet} List of commands`, value: commandString })
+        .addFields({ name: `<:info:1114861006502498376> ${bullet} List of commands`, value: `${commandString}` })
+        .addFields({ name: `${success} ${bullet} Setup guide`, value: guide})
+        .addFields({ name: `Important things to note`, value: `We will **never** ask for your account passwords! Only visit urls that start with \`roauth.xyz\` we will never redirect you anywhere else! The real Roauth bot will always have <:verif:1114867801312731146> next to its name!`})
         .setColor("RANDOM")
         .setImage("https://media.tenor.com/ZNi18lLfqs4AAAAC/rainbow-line-line.gif")
         .setTimestamp()
